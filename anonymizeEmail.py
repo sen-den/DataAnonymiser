58993f9048ee4e5cdbbcb649fd9f62b0d4e3fc09
SenDen <senden1998@gmail.com>
2019-05-19 14:09:24 +0300

    Mask short emails

--- anonymizeEmail.py
+++ anonymizeEmail.py
@@ -7,7 +7,13 @@ def anonymizeEmail(text):
 
 	# replace each part between first and last character of valid email 
 	# by same size string of substitute characters
-	text = re.sub(r'(?<=[\w])([\w][\w\_\.\-\+]*)(?=[\w]\@[\w]+(\.[\w][\w\-\+]*[\w])+)'
+	text = re.sub(r'(?<=[\w])([\w\_\.\-\+]*)(?=[\w]\@[\w]+(\.[\w][\w\-\+]*[\w])+)'
+		, lambda match: subsSign * len(match.group(1))
+		, text)
+
+	# special condition for email`s usernames which consist only of 1-2 characters
+	# masks them completely
+	text = re.sub(r'(?<!\S)([\w]{1,2})(?=\@[\w]+(\.[\w][\w\-\+]*[\w])+)'
 		, lambda match: subsSign * len(match.group(1))
 		, text)
 
