d2ee11c07dcc27af81edef1795e594b51e03193a
SenDen <senden1998@gmail.com>
2019-05-19 15:05:28 +0300

    Command line parametr added

--- anonymize.py
+++ anonymize.py
@@ -1,15 +1,23 @@
 import sys
+import re
 
 from anonymizeSkype import anonymizeSkype
 from anonymizePhone import anonymizePhone
 from anonymizeEmail import anonymizeEmail
 
-if (len(sys.argv) < 3):
-	 print("Pass input and output file names to script as command line arguments!")
-	 sys.exit(1)
+if (len(sys.argv) < 4):
+	print("Pass command, input and output file names to script as command line arguments!")
+	print("For example: python3 ep i.txt o.txt")
+	sys.exit(1)
 
-inputFileName = sys.argv[1]
-outputFileName = sys.argv[2]
+anonymizeCommand = sys.argv[1]
+
+if(re.search("[^eps]", anonymizeCommand) != None):
+	print("Unknown commands, use one of e, p, s or their combination.")
+	sys.exit(1)
+
+inputFileName = sys.argv[2]
+outputFileName = sys.argv[3]
 
 try:
 	with open(inputFileName, "r") as inpuFile:
@@ -31,9 +39,12 @@ except OSError:
 with open(inputFileName, "r") as inpuFile:
 	text = inpuFile.read()
 
-	text = anonymizeSkype(text)
-	text = anonymizePhone(text)
-	text = anonymizeEmail(text)
+	if(re.search("[e]", anonymizeCommand) != None):
+		text = anonymizeEmail(text)
+	if(re.search("[s]", anonymizeCommand) != None):
+			text = anonymizeSkype(text)
+	if(re.search("[p]", anonymizeCommand) != None):
+		text = anonymizePhone(text)
 
 	with open(outputFileName, "w") as outputFile:
 		outputFile.write(text)
